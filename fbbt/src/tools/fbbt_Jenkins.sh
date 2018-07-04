40be4b43b79086eac3a5de8980e16b9f6a1e8ae2
dosumis <dosumis@gmail.com>
2018-07-04 11:58:31 +0100

    * Updates to Jenkins shell script driving release:
      * Cleaned up flow by directly generating OBO files at each step where needed
      * Added perl one-liner to fixe namespace issue arising from merge
      * Removed GO relationships from whitelist generating fly_anatomy.obo

--- fbbt/src/tools/fbbt_Jenkins.sh
+++ fbbt/src/tools/fbbt_Jenkins.sh
@@ -14,39 +14,45 @@ update_EC_defs.pl tmp.obo > tmp2.obo
 echo ''
 
 echo '*** Merging accessory files ***'
-robot merge --input fbbt/src/ontology/fbbt_auth_attrib_licence.owl --input tmp2.obo -o tmp.owl
+owltools fbbt/src/ontology/fbbt_auth_attrib_licence.owl --merge tmp2.obo -o -f obo tmp3.obo
 
-tmp.obo
-tmp2.obo # Cleaning up
+# Fix namespace issues caused by merge:
+
+cat tmp3.obo | perl -p -e  's/namespace: fly_anatomy.ontology\n//' | perl -p -e 's/^ontology: fbbt$/ontology: fbbt\ndefault-namespace: fly_anatomy.ontology/' > tmp4.obo
+
+# Add step to add date automatically here?  Could be set from 
+
+#rm tmp.obo
+#rm tmp2.obo # Cleaning up
 
 echo ''
 echo "*** Generating release files using the $REASONER reasoner ***"
 echo ''
-ontology-release-runner --reasoner $REASONER tmp.owl  --no-subsets --simple --relaxed --asserted --allow-overwrite --outdir oort
+ontology-release-runner --reasoner elk tmp4.obo  --no-subsets --simple --relaxed --asserted --allow-overwrite --outdir oort
 
-rm tmp.owl # Cleaning up
+# rm tmp3.obo # Cleaning up
 echo ''
 echo "*** Generating obograph JSON version***"
 owltools oort/fbbt.owl -o -f json oort/fbbt.json
 echo ''
 echo "*** Filtering relations to generating basic & FB versions ***"
-owltools oort/fbbt-simple.obo --make-subset-by-properties part_of develops_from // -o file://`pwd`/oort/fbbt-basic.owl  # Basic avoids properties that might => cycles
+owltools oort/fbbt-simple.obo --make-subset-by-properties part_of develops_from // -o -f obo oort/fbbt-basic.obo  # Basic avoids properties that might => cycles
 
-export FB_REL_WL="connected_to develops_directly_from develops_from electrically_synapsed_to fasciculates_with has_part has_postsynaptic_terminals_in has_presynaptic_terminals_in has_soma_location has_synaptic_terminals_in has_synaptic_terminals_of innervated_by innervates overlaps part_of partially_overlaps synapsed_by synapsed_by_type_III_bouton_of synapsed_by_type_II_bouton_of synapsed_by_type_Ib_bouton_of synapsed_by_type_Is_bouton_of positively_regulates regulates releases_neurotransmitter secretes_hormone located_in negatively_regulates occurs_in inheres_in has_quality has_function_in_part_of has_function_in expresses contained_in composed_primarily_of capable_of_part_of capable_of synapsed_to synapsed_via_type_III_bouton_to synapsed_via_type_II_bouton_to synapsed_via_type_Ib_bouton_to synapsed_via_type_Is_bouton_to tracheates //"
+export FB_REL_WL="connected_to develops_directly_from develops_from electrically_synapsed_to fasciculates_with has_part has_postsynaptic_terminals_in has_presynaptic_terminals_in has_soma_location has_synaptic_terminals_in has_synaptic_terminals_of innervated_by innervates overlaps part_of partially_overlaps synapsed_by synapsed_by_type_III_bouton_of synapsed_by_type_II_bouton_of synapsed_by_type_Ib_bouton_of synapsed_by_type_Is_bouton_of releases_neurotransmitter secretes_hormone located_in inheres_in has_quality has_function_in_part_of has_function_in expresses contained_in composed_primarily_of capable_of_part_of capable_of synapsed_to synapsed_via_type_III_bouton_to synapsed_via_type_II_bouton_to synapsed_via_type_Ib_bouton_to synapsed_via_type_Is_bouton_to tracheates //"
 #obolib-owl2obo oort/fbbt-basic.owl -o oort/fbbt-basic.obo 
 # TEMJ commented above line 20170906 because Java keeps throwing "NullPointerException" errors upon obolib use after moving from clara to flybase-vm machine. Changing to use owltools instead, which has a converter of owl to obo. May want to update in the future to use ROBOT or whatever tool becomes standard. 20170906.
-owltools oort/fbbt-basic.owl -o -f obo oort/fbbt-basic.obo #TEMJ added 20170906 to replace obolib command.
-rm oort/fbbt-basic.owl  # Cleaning up.  No point in keeping OWL version
-owltools oort/fbbt-simple.obo --make-subset-by-properties $FB_REL_WL -o file://`pwd`/tmp.owl
+#owltools oort/fbbt-basic.owl -o -f obo oort/fbbt-basic.obo #TEMJ added 20170906 to replace obolib command.
+#rm oort/fbbt-basic.owl  # Cleaning up.  No point in keeping OWL version
+owltools oort/fbbt-simple.obo --make-subset-by-properties $FB_REL_WL -o -f obo tmp5.obo
 #obolib-owl2obo tmp.owl -o oort/fbbt-flybase.obo
 # TEMJ commented above line 20170906 for same reason as above.
 echo ''
 echo '***Building FlyBase version***'
 echo ''
-owltools tmp.owl -o -f obo tmp2.obo #TEMJ added 20170906 to replace obolib command.
-cat tmp2.obo | sed 's/^xref: OBO_REL:part_of/xref_analog: OBO_REL:part_of/' | sed 's/^xref: OBO_REL:has_part/xref_analog: OBO_REL:has_part/' > oort/fly_anatomy.obo  # Perhaps just make a generic substitution for xref: OBO_REL ?
-rm tmp.owl  # Cleaning up
-rm tmp2.obo # Cleaning up
+# owltools tmp5.obo -o -f obo tmp2.obo #TEMJ added 20170906 to replace obolib command.
+cat tmp5.obo | sed 's/^xref: OBO_REL:part_of/xref_analog: OBO_REL:part_of/' | sed 's/^xref: OBO_REL:has_part/xref_analog: OBO_REL:has_part/' > oort/fly_anatomy.obo  # Perhaps just make a generic substitution for xref: OBO_REL ?
+#rm tmp.owl  # Cleaning up
+#rm tmp2.obo # Cleaning up
 echo ''
 echo '*** Running tests on new fbbt-simple.obo ***'
 echo '*** chado load checks ***'
