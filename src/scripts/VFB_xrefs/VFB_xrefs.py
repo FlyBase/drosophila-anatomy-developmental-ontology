577691606a3769e8dca8eff06a4f23dad64ba065
Clare72 <cp390@cam.ac.uk>
2021-04-19 14:55:40 +0100

    moving code for VFB linkouts from patterns to scripts, adding instructions

---
+++ src/scripts/VFB_xrefs/VFB_xrefs.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+"""
+Makes a template for adding VFB xrefs to classes that are part_of some 'nervous system'.
+"""
+
+import requests
+import json
+import pandas as pd
+from collections import OrderedDict
+
+fbbt = json.load(open("../ontology/fbbt.json", "r"))
+
+#find all nervous systems and parts of them - nervous system = FBbt_00005093
+
+terms = ['http://purl.obolibrary.org/obo/FBbt_00005093']
+result = ['http://purl.obolibrary.org/obo/FBbt_00005093']
+x = 1
+while x > 0:
+    x = 0
+    new_terms = list()
+    for e in fbbt['graphs'][0]['edges']:
+        if (e['obj'] in terms) and ((e['pred'] == 'http://purl.obolibrary.org/obo/BFO_0000050') \
+                                     or (e['pred'] == 'is_a')):
+            new_terms.append(e['sub'])
+            x += 1
+    terms.clear()
+    for i in new_terms:
+        if i not in result:
+            result.append(i)
+            terms.append(i)
+    new_terms.clear()
+
+# remove hemibrain provisional cell types
+
+result = [r for r in result if not 'FBbt_2' in r]
+
+# remove provisional Feng neurons
+
+feng_ids = ['http://purl.obolibrary.org/obo/FBbt_000' + n for n in [str(i) for i in range(51089, 51172)]]
+result = [r for r in result if not r in feng_ids]
+
+"""
+# to output list of results - not usually needed
+
+result_df = pd.DataFrame(result)
+result_df.to_csv("./result_list.tsv", sep = "\t", header=False, index=False)
+"""
+
+#generate VFB refs
+VFB_dict = OrderedDict([])
+for i in result:
+    fb_id = i.split("/")[-1]
+    VFB_dict[i] = "VFB:" + fb_id
+
+# Make a dictionary with key - column header & value = template specification (first row of table).
+# Make first two columns
+
+template_seed = OrderedDict([ ('ID' , 'ID'), ("Xref" , "A oboInOwl:hasDbXref")])
+
+# Create dataFrame for template
+# from_records takes a list of dicts - one for each row.  We only have one row.
+
+template = pd.DataFrame.from_records([template_seed])
+
+for i in VFB_dict:
+
+    row_od = OrderedDict([]) #new template row as an empty ordered dictionary
+    for c in template.columns: #make columns and blank data for new template row
+        row_od.update([(c , "")])
+    
+    #ID and xref
+    row_od['ID'] = i
+    row_od["Xref"] = VFB_dict[i]
+    
+    #make new row into a DataFrame and add it to template
+    new_row = pd.DataFrame.from_records([row_od])
+    template = pd.concat([template, new_row], ignore_index=True, sort=False)
+    
+template.to_csv("./VFB_template.tsv", sep = "\t", header=True, index=False)
