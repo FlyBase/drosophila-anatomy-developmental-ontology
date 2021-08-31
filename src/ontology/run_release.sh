07ec88289839701562e0981dbefc742dde078c93
Clare72 <cp390@cam.ac.uk>
2021-08-31 14:09:32 +0100

    switch PAT=false

--- src/ontology/run_release.sh
+++ src/ontology/run_release.sh
@@ -5,12 +5,12 @@
 
 # 2. Next lets run the preprocessing. This involves creating creating the definitions (essentially substitution of the ones containing the $sub_GO:001 macro)
 # This process results in an updated source file dpo-edit-release.owl
-sh run.sh make pre_release -B
+sh run.sh make PAT=false pre_release -B
 
 # 3. Now lets run the proper release. Note that here, we are overwriting the SRC variable to be the newly created dpo-edit-release.owl
 # This process generates everything from the simple and basic releases to the various flybase reports
 # All deviations from the standard OBO process can be found in the dpo.Makefile file
-sh run.sh make SRC=fbbt-edit-release.owl prepare_release -B
+sh run.sh make PAT=false SRC=fbbt-edit-release.owl prepare_release -B
 
 # 4. Run some post release steps
-sh run.sh make post_release -B
+sh run.sh make PAT=false post_release -B
--- src/ontology/travis.sh
+++ src/ontology/travis.sh
@@ -1,8 +1,8 @@
 # Running the DPO release pipeline for TRAVIS
 set -e
 
-sh run.sh make IMP=false pre_release -B
+sh run.sh make IMP=false PAT=false pre_release -B
 
-sh run.sh make SRC=fbbt-edit-release.owl IMP=false prepare_release -B
+sh run.sh make SRC=fbbt-edit-release.owl IMP=false PAT=false prepare_release -B
 
-sh run.sh make IMP=false flybase_qc -B
+sh run.sh make IMP=false PAT=false flybase_qc -B
