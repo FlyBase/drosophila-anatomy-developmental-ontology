88283d14c7315b9e12a3c6610ef3943127852def
Clare72 <cp390@cam.ac.uk>
2022-06-09 12:48:36 +0100

    release files 2022-06-09

--- src/scripts/obo_spellchecker.py
+++ src/scripts/obo_spellchecker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # obo-spellchecker - Spellchecker for OBO-formatted ontologies
-# Copyright © 2021 Damien Goutte-Gattat
+# Copyright © 2021,2022 Damien Goutte-Gattat
 #
 # Redistribution and use of this script, with or without modifications,
 # is permitted provided that the following conditions are met:
@@ -25,6 +25,7 @@
 
 import sys
 from subprocess import run
+from re import compile
 
 from pronto import Ontology
 from spellchecker import SpellChecker
@@ -45,97 +46,225 @@ def die(msg):
     sys.exit(1)
 
 
-class OntoChecker(object):
+class OntologyTermChecker(object):
+    """Checks a single ontology term.
+    
+    This is the base class for implementing custom checks on ontology
+    terms. Derived class should implement the :meth:`get_errors` method
+    which performs the actual check.
+    """
 
-    def __init__(self):
-        self._checker = SpellChecker()
-        self._post_filters = []
+    def __init__(self, name, fields=['definition', 'comment']):
+        """Creates a new instance.
+        
+        :param name: the name of the check, as will be displayed in the
+            final report
+        :param fields: the list of fields within a term the check should
+            be applied to
+        """
+
+        self._name = name
+        self._fields = fields
         self._pre_filters = []
+        self._post_filters = []
 
-    def add_custom_dictionary(self, dictionary):
-        self._checker.word_frequency.load_text(dictionary)
+    @property
+    def name(self):
+        """The name of this check.
+        
+        This name will be displayed in front of every violation in the
+        generated report.
+        """
 
-    def add_filter(self, filter_, pre=False):
-        if pre:
-            self._pre_filters.append(filter_)
-        else:
-            self._post_filters.append(filter_)
+        return self._name
+
+    @property
+    def fields(self):
+        """A list of field names the check should be applied to."""
+
+        return self._fields
+
+    @property
+    def pre_filters(self):
+        """A list of input filters.
+        
+        Those filters are applied to each text value of a term before
+        the :meth:`get_errors` method is called. They can modify the
+        value as needed (e.g. to remove words or patterns that would
+        yield false positives). Each filter should be a function taking
+        a string as argument and returning the modified string.
+        """
+        return self._pre_filters
+
+    @property
+    def post_filters(self):
+        """A list of output filters.
+        
+        Those filters are applied to the list of errors as returned by
+        the :meth:`get_errors` method. They can exclude an error from
+        the list (e.g. to remove a false positive). Each filter should
+        be a function taking a string as argument (an error found in a
+        text value) and returning True to exclude that error, or False
+        to keep the error as it is.
+        """
+
+        return self._post_filters
 
     def check_term(self, term):
-        n = 0
+        """Checks a single ontology term.
+        
+        This is the main client interface. Called with an ontology term,
+        it returns a dictionary whose keys are term fields (`name`,
+        `definition`, `comment`, or `synonyms`) and values are lists of
+        errors found in the corresponding fields.
+        
+        :param term: the ontology term to check
+        :return: a dictionary of errors for each term field
+        """
+
         results = {}
 
-        for field in ['name', 'definition', 'comment']:
-            words = self._check_term_field(term, field)
-            if words is not None:
-                n += 1
-                results[field] = words
+        for field in self.fields:
+            value = getattr(term, field)
+            if value is None:
+                continue
 
-        words = self._check_term_synonyms(term)
-        if words is not None:
-            n += 1
-            results['synonyms'] = words
+            if field == 'synonyms':
+                result = self.check_synonyms(value)
+            else:
+                result = self.check_value(value)
+            if len(result) > 0:
+                results[field] = result
 
-        if n > 0:
-            return results
-        else:
-            return None
+        return results
 
-    def _check_term_field(self, term, field):
-        value = getattr(term, field)
-        if value is None:
-            return None
+    def check_synonyms(self, synonyms):
+        """Checks all the synonyms of a term."""
 
-        words = self._check_value(value)
-        if len(words):
-            return words
-        else:
-            return None
+        result = []
+        for synonym in synonyms:
+            result.extend(self.check_value(synonym.description))
+        return result
 
-    def _check_term_synonyms(self, term):
-        words = []
-        for synonym in term.synonyms:
-            words.extend(self._check_value(synonym.description))
+    def check_value(self, value):
+        """Checks a single text value.
+        
+        This method takes care of applying the pre- and post-filters so
+        that the derived classes do not have to do it.
+        """
 
-        if len(words):
-            return words
-        else:
-            return None
+        for f in self.pre_filters:
+            value = f(value)
 
-    def _check_value(self, value):
-        input_words = self._checker.split_words(value)
-        input_words = [w for w in input_words if not self._apply_filters(w, self._pre_filters)]
+        result = self.get_errors(value)
+        return [r for r in result if not self.apply_output_filters(r)]
 
-        words = self._checker.unknown(input_words)
-        words = [w for w in words if not self._apply_filters(w, self._post_filters)]
+    def get_errors(self, value):
+        """Get all errors in the given text value.
+        
+        This method should return a list of errors.
+        """
+        pass
 
-        return words
+    def apply_output_filters(self, value):
+        """Check an error against all output filters."""
 
-    def _apply_filters(self, word, filters):
-        for f in filters:
-            if f(word):
+        for f in self.post_filters:
+            if f(value):
                 return True
         return False
 
 
-def exclude_words_with_number(word):
-    return not word.isalpha()
+class FullStopChecker(OntologyTermChecker):
+    """Checks that definitions and comments end with a full stop."""
+
+    def __init__(self):
+        OntologyTermChecker.__init__(self, 'missing final full stop')
+
+    def get_errors(self, value):
+        if len(value) >= 1 and value[-1] not in '.?]':
+            return [value.split(' ')[-1]]
+        else:
+            return []
 
 
-def exclude_all_uppercase_words(word):
-    return word.isupper()
+class PatternChecker(OntologyTermChecker):
+    """A generic check based on a regular expression."""
+
+    def __init__(self, name, pattern, fields=['definition', 'comment']):
+        OntologyTermChecker.__init__(self, name, fields)
+        self._pattern = compile(pattern)
+
+    def get_errors(self, value):
+        return [f'"{e}"' for e in self._pattern.findall(value)]
 
 
-def exclude_camelcase_words(word):
-    return word[0].islower() and not word[1:].islower()
+class RepeatedWordsChecker(PatternChecker):
+
+    def __init__(self):
+        PatternChecker.__init__(self, 'repeated words', '\\b(\w+)\s+\\1\\b')
 
 
-def make_exclude_short_word_filter(threshold):
+class RepeatedSpacesChecker(PatternChecker):
 
-    def exclude_short_word_filter(word):
-        return len(word) < threshold
+    def __init__(self):
+        PatternChecker.__init__(self, 'repeated spaces', '(\s)\\1+')
 
-    return exclude_short_word_filter
+    def get_errors(self, value):
+        m = self._pattern.findall(value)
+        if len(m) > 0:
+            return [f"{len(m)} repeated whitespace(s)"]
+        else:
+            return []
+
+
+class RepeatedPunctuationChecker(PatternChecker):
+
+    def __init__(self):
+        PatternChecker.__init__(self, 'repeated punctuation',
+                                '(?:[^.]\.\.(?:[^.]|\Z))|(?:,,)|(?:\?\?)|(?:!!)|(?:;;)')
+
+
+class TermSpellChecker(OntologyTermChecker):
+
+    def __init__(self):
+        OntologyTermChecker.__init__(self, 'misspelling',
+                                     fields=['name', 'definition', 'comment',
+                                             'synonyms'])
+        self._checker = SpellChecker()
+        self._short_word_threshold = 4
+        self._post_filters = [self._exclude_words_with_number,
+                              self._exclude_short_word_filter]
+        self._input_word_filters = [self._exclude_all_uppercase_words,
+                                    self._exclude_camelcase_words]
+
+    def add_custom_dictionary(self, dictionary):
+        self._checker.word_frequency.load_text(dictionary)
+
+    def get_errors(self, value):
+        words = self._checker.split_words(value)
+        words = [w for w in words if not self._apply_word_filters(w)]
+
+        errors = self._checker.unknown(words)
+        return errors
+
+    def _apply_word_filters(self, word):
+        for f in self._input_word_filters:
+            if f(word):
+                return True
+        return False
+
+    def _exclude_words_with_number(self, word):
+        return not word.isalpha()
+
+    def _exclude_all_uppercase_words(self, word):
+        return word.isupper()
+
+    def _exclude_camelcase_words(self, word):
+        return word[0].islower() and not word[1:].islower()
+
+    def _exclude_short_word_filter(self, word):
+        return len(word) < self._short_word_threshold
 
 
 def _load_dictionary(location):
@@ -179,34 +308,40 @@ def check_ontology(obofile, output, dictionary, obsolete):
     try:
         onto = Ontology(obofile)
     except Exception as e:
-        die(f"Cannot load ontology: {e}")
+        raise RuntimeError(f"Cannot load ontology: {e}")
 
-    checker = OntoChecker()
+    spell_checker = TermSpellChecker()
     for dictfile in dictionary:
         try:
             dictdata = _load_dictionary(dictfile)
         except Exception as e:
-            die(f"Cannot load dictionary: {e}")
-        checker.add_custom_dictionary(dictdata)
+            raise RuntimeError(f"Cannot load dictionary: {e}")
+        spell_checker.add_custom_dictionary(dictdata)
 
-    checker.add_filter(exclude_words_with_number)
-    checker.add_filter(make_exclude_short_word_filter(4))
-    checker.add_filter(exclude_all_uppercase_words, pre=True)
-    checker.add_filter(exclude_camelcase_words, pre=True)
+    checkers = [spell_checker,
+                FullStopChecker(),
+                RepeatedWordsChecker(),
+                RepeatedSpacesChecker(),
+                RepeatedPunctuationChecker()]
 
     for term in sorted(onto.terms()):
         if term.obsolete and not obsolete:
             continue
-        r = checker.check_term(term)
-        if not r:
-            continue
 
-        output.write(f"Term: {term.name} ({term.id})\n")
-        for k, v in r.items():
-            output.write(f"In {k}: ")
-            output.write(" ".join(sorted(v)))
+        term_shown = False
+
+        for checker in checkers:
+            r = checker.check_term(term)
+            if len(r) == 0:
+                continue
+
+            if not term_shown:
+                output.write(f"Term: {term.name} ({term.id})\n")
+            for k, v in r.items():
+                output.write(f"{checker.name}: in {k}: ")
+                output.write(" ".join(sorted(v)))
+                output.write("\n")
             output.write("\n")
-        output.write("\n")
 
 
 if __name__ == '__main__':
