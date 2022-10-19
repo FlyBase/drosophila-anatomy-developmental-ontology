a879390905863ed743e6b4cb52aba1bf55c3ca7c
Clare72 <cp390@cam.ac.uk>
2022-10-19 14:26:07 +0100

    release files 2022-10-19

--- src/scripts/fetch_authors.py
+++ src/scripts/fetch_authors.py
@@ -3,23 +3,9 @@
 # fetch_flybase_authors - Get authors known to FlyBase
 # Copyright © 2021 Damien Goutte-Gattat
 #
-# Redistribution and use of this script, with or without modifications,
-# is permitted provided that the following conditions are met:
-#
-# 1. Redistributions of this script must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
-# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+# This file is part of the flybase-ontology-scripts distribution and is
+# distributed under the terms of the MIT license. See the LICENSE.md
+# file in the distribution for detailed conditions.
 
 # pypi-requirements: psycopg2 click
 
