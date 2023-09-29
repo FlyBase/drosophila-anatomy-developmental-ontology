54d687cfcf93a69ef94adbcd2ce4f9b76812276a
Clare72 <cp390@cam.ac.uk>
2023-09-29 16:19:12 +0100

    also use id field for existing typedefs

--- src/scripts/FB_typedefs.py
+++ src/scripts/FB_typedefs.py
@@ -15,13 +15,15 @@ for file in os.listdir('.'):
 ontology = oaklib.get_adapter(ontology_file)
 all_typedefs = [t for t in ontology.entities(owl_type='owl:ObjectProperty')]
 
-# 2. Read existing additions file and check what is missing
+# 2. Read existing additions file and check what is missing (id could be 'id' or 'xref')
 if os.path.isfile(typedefs_file):
     with open(typedefs_file, 'r') as file:
         flybase_additions_lines = [l for l in file.readlines()]
 
     existing_typedefs = [re.match('^xref:[ ](.*)', l).group(1) for l in flybase_additions_lines \
                             if re.match('^xref:[ ](.*)', l)]
+    existing_typedefs += [re.match('^id:[ ](.*)', l).group(1) for l in flybase_additions_lines \
+                            if re.match('^id:[ ](.*)', l)]
 else:
     flybase_additions_lines = ['format-version: 1.2\n', 'ontology: %s\n' % ont_name, '\n']
     existing_typedefs = []
@@ -33,7 +35,8 @@ new_typedefs = [t for t in all_typedefs if not (t in existing_typedefs)]
 for t in new_typedefs:
     flybase_additions_lines += '[Typedef]\n'
     flybase_additions_lines += 'id: %s\n' % ontology.label(t).replace(' ', '_')
-    flybase_additions_lines += 'xref: %s\n\n' % t
+    flybase_additions_lines += 'xref: %s\n' % t
+    flybase_additions_lines += '\n'
 
 with open(typedefs_file, "w") as file:
     file.writelines(flybase_additions_lines)
